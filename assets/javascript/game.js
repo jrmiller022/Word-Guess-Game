809f600e7ed5506eedaf4f32745dadc50866193d
Jaybird <jrmiller022@gmail.com>
2018-12-05 22:52:00 -0600

    work guess game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,127 +3,47 @@ var names = ["Mango", "Apple", "Orange", "Banana", "Strawberries"];
 
 //Varibles to store
 var lettersOfWords = [];
-var randomWord = "_";
+var randomWords = "_";
 var blanks = 0;
 var blanksAndCorrect = [];
 var wrongGuess = [];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 var wins = 0;
-var loss = 0;
+var losses = 0;
 var guessesRemaining = 9;
 
 //functions
 function Game() {
 
-//computer creates random words
-randomWord = words[Math.floor(Math.random() * words.length)];
-    console.log(randomWord);
+    //have player press any key to get started
 
-//split the words into seperate arrays
-lettersofWords = randomWord.split("_");
+    //player picks a random word
+    randomWords = names[Math.floor(Math.random() * names.length)];
+    console.log(names);
 
-//stored blank words
-blanks = lettersofWords.length;
+    //split the words into seperate arrays
+    lettersOfWords = randomWords.split("_");
 
-//creating for loops for each letter stored
-for (var i = 0; i < blanks; i++) {
-    blanksAndCorrect.push("_");
-    console.log(blanksAndCorrect[i]);
-};
+    //stored blank words
+    blanks = lettersOfWords.length;
 
-//show the inner html
-document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
-
-//console logging 
-console.log(randomWord);
-console.log(lettersOfWord);
-console.log(blanks);
-console.log(blanksAndCorrect);
-};
-//RESET FUNCTION
-//__________________________________________________________
-function reset() {
-    guessesRemaining = 9;
-    wrongGuess = [];
-    blanksAndCorrect = [];
-    Game()
-};
-
-//__________________________________________________________
-//CHECK LETTERS/COMPARE FUNCTION
-//__________________________________________________________
-
-//If/Else, to see if letter selected matches random word
-function checkLetters(letter) {
-    var letterInWord = false;
-    //if the generated randomword is equal to the letter entered... then variable is true
+    //creating for loops for each letter stored
     for (var i = 0; i < blanks; i++) {
-        if (randomWord[i] == letter) {
-            letterInWord = true;
-        };
+        blanksAndCorrect.push("_");
+        
     };
-    //if letterInWord (false)
-    if (letterInWord) {
-        //check each letter to see if it matches word
-        for (var i = 0; i < blanks; i++) {
-            if (randomWord[i] == letter) {
-                blanksAndCorrect[i] = letter;
-            }
-        }
-    }
-    //otherwise, push the incorrect guess in the wrong guesses section, and reduce remaining guesses
-    else {
-        wrongGuess.push(letter);
-        guessesRemaining--;
-    };
-    console.log(blanksAndCorrect);
+
+    //show the inner html
+    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join("  ");
+
+    //console.logs
 };
 
-//__________________________________________________________
-//FINAL COMPLETE FUNCTION
-//__________________________________________________________
+    //how many guesses player has left
 
-//check to see if player won...
-function complete() {
-    console.log("wins:" + wins + "| losses:" + losses + "| guesses left:" + guessesRemaining)
+    //Let player know how many guessed letters are wrong
 
-    //if WON...then alert, play audio, display image and reset new round
-    if (lettersOfWord.toString() == blanksAndCorrect.toString()) {
-        wins++;
-        aud()
-        reset()
-        //display wins on screen
-        document.getElementById("winstracker").innerHTML = " " + wins;
+    //If the player guess the right word, Tell the player that they won
 
-        //if LOST...then alert and reset new round
-    } else if (guessesRemaining === 0) {
-        losses++;
-        reset()
-        document.getElementById("image").src = "./assets/images/try-again.png"
-        document.getElementById("losstracker").innerHTML = " " + losses;
-    };
-    //display losses on screen && guesses remaining countdown
-    document.getElementById("currentword").innerHTML = "  " + blanksAndCorrect.join(" ");
-    document.getElementById("guessesremaining").innerHTML = " " + guessesRemaining;
-};
-
-// EXECUTE CODE 
-//_____________________________________________________
-
-//call start game function
-Game()
-
-//check for keyup, and convert to lowercase then store in guesses
-document.onkeyup = function (event) {
-    var guesses = String.fromCharCode(event.keyCode).toLowerCase();
-    //check to see if guess entered matches value of random word
-    checkLetters(guesses);
-    //process wins/loss 
-    complete();
-    //store player guess in console for reference 
-    console.log(guesses);
-
-    //display/store incorrect letters on screen
-    document.getElementById("playerguesses").innerHTML = "  " + wrongGuess.join(" ");
-};
+    //End game if player loses and runs out of guesses. 
